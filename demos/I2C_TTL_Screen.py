58dd68122d459c06e3d52646589506322e6e2379
anthony <secyson@gmail.com>
2022-04-22 16:19:10 +0800

    I2C Screen

---
+++ demos/I2C_TTL_Screen.py
@@ -0,0 +1,16 @@
+'''
+Description: This is a demo for ESP32 using UART TTL Screen
+Screen Model: TJC4832T135_011N/R/C TJC4832T135_011N/R_Z01
+Screen Wiki: http://wiki.tjc1688.com/doku.php
+Author: @Antsec
+'''
+
+from machine import UART
+import time
+
+while True:
+    
+    uart1 = UART(1, baudrate=115200, tx=7, rx=8)
+    uart1.write('t0.txt="Hi DiaoMao"')
+    uart1.write(b'\xff\xff\xff') #Ending Tag Hex
+    time.sleep(1)
